39669e562ca4bb27a0bddc2098a83956d681b15d
Baurain Olivier <baurain.o@gmail.com>
2022-02-22 15:57:58 +0100

    ahead

---
+++ packages/core/base/src/adapter.ts
@@ -0,0 +1,6 @@
+
+export abstract class BaseAdapter {
+  abstract id: string;
+  abstract name: string;
+  abstract url: string;
+}
--- packages/core/base/src/types.ts
+++ packages/core/base/src/types.ts
@@ -1,1 +1,2 @@
 export type AdapterName = string & { __brand__: 'AdapterName' };
+export type AdapterId = string & { __brand__: 'AdapterId' };
